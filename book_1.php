c8917ae229ce1dfe57812f7e2abf92a1ccd7baf5
subho1999 <ghosh.subho.1999@gmail.com>
2020-06-06 04:14:28 +0530

    Book_1 started

--- book_1.php
+++ book_1.php
@@ -1,75 +1,11 @@
+<?php
+  session_start();
+?>
 <!DOCTYPE html>
 <html>
   <head>
     <title>Communication Wizard</title>
-    <style>
-      html{
-        margin: 0;
-        padding: 0;
-      }
-      body{
-        font-family:Bahnschrift Light;
-        text-align: center;
-        width: 100%;
-        margin: 0;
-        padding: 0;
-      }
-      .heading{
-        display: flex;
-        justify-content: space-between;
-        background-color: #404040;
-        color: white;
-        padding: 1%;
-      }
-      .topnav {
-      background-color: #333;
-      overflow: hidden;
-      }
-      .topnav a {
-      float: left;
-      color: #f2f2f2;
-      text-align: center;
-      padding: 14px 16px;
-      text-decoration: none;
-      font-size: 125%; 
-      }
-      .topnav a:hover {
-      background-color: rgb(109, 109, 109);
-      color: black;
-      }
-      .topnav a.active {
-      background-color: rgb(72, 73, 72);
-      color: white;
-      margin-left: 660px;
-      margin-top:10px;
-      }
-      #abc{
-        margin-left: 40%;
-      }
-      .ab{
-        margin-top:10px;
-      }
-      .innovation{
-        background-color: black;
-        color: white;
-        font-size: 200%;
-      }
-      .columns img{
-        width: 50%;
-      }
-      .main-slideshow-container{
-                  position: relative;
-                  background-image: url("img/8.jpg");
-                  border-radius: 5px;
-      } 
-      .box h2{
-        color: black;
-      }
-      .box hr{
-        border: 1px solid black;
-      }
-    
-    </style>
+    <link rel="stylesheet" href="styles/book_1.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.css">
   </head>
   <body>  
@@ -80,18 +16,56 @@
       <a class="ab" href="faqs.php">FAQs</a>
       <a class="ab" href="books.php">Books</a>	
       <a class="ab" href="qp.php">Question Papers</a>
-      <a class="ab" href="login.php">Login</a>
-      <a class="ab" href="signup.php">Signup</a>
-    </div>
-
-    <div class="box is-bordered">
-      <hr>
-      <h2 class="subtitle">Basic Electrical and Electronics Engineering</h2>
       <?php
-
-        
-
+        if(isset($_SESSION['loggedin'])){
+          echo '<a class="ab" href="signout.php">Welcome '.$_SESSION['loggedin'].'</a>';
+        }
+        else {
+          echo '<a class="ab" href="login.php">Login</a>
+                <a class="ab" href="signup.php">Signup</a>';
+        }
       ?>
     </div>
+      
+    <!-- <div class="box is-bordered">
+      <form action="book.upload.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="book">
+        <input type="submit" name="bookupload" value="Upload">
+      </form>
+    </div> -->
+    <div class="box is-bordered">
+      <button id="upload-modal-btn">Upload Books</button>
+    </div>
+    
+    <div class="modal">
+      <div class="modal-content">
+        <span class="close-btn">&times;</span>
+        <p>Some text</p>
+      </div>
+    </div>
+
+    <hr>
+    <div>
+      <ul>
+        <li>item1</li>
+        <li>item2</li>
+      </ul>
+    </div>
   </body>
+  <script>
+    let modalBtn = document.getElementById("upload-modal-btn")
+    let modal = document.querySelector(".modal")
+    let closeBtn = document.querySelector(".close-btn")
+    modalBtn.onclick = function(){
+      modal.style.display = "block"
+    }
+    closeBtn.onclick = function(){
+      modal.style.display = "none"
+    }
+    window.onclick = function(e){
+      if(e.target == modal){
+        modal.style.display = "none"
+      }
+    }
+  </script>
 </html>
