8de8fc50f627c7c85544a9e07070b3f9394f71d1
subho1999 <ghosh.subho.1999@gmail.com>
2020-06-06 03:11:18 +0530

    Signup.inc changes

--- signup.inc.php
+++ signup.inc.php
@@ -11,7 +11,7 @@ if(isset($_POST['signup-submit'])){
         exit();
     }
 
-    require 'dbh.inc.php';
+    require_once 'db_connect.php';
 
     $name = $_POST['name'];
     $regno = $_POST['reg'];
@@ -22,19 +22,12 @@ if(isset($_POST['signup-submit'])){
 
     if(empty($name) || empty($regno) || empty($email) || empty($username) || empty($password) || empty($branch)){
         header("Location: signup.php?signup=emptyfields");
-        exit();
     }
-    // elseif(!filter_var($email, FILTER_VALIDATE_EMAIL) && !preg_match("/^[a-zA-Z0-9]*$/", $username)){
-    //     header("Location: signup.php?error=invalidemailusername");
-    //     exit();
-    // }
     elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)){
         header("Location: signup.php?signup=invalidemail&name=$name&regno=$regno&branch=$branch");
-        exit();
     }
     elseif(!preg_match("/^[a-zA-Z0-9]*$/", $username)){
         header("Location: signup.php?signup=invalidusername&name=$name&regno=$regno&email=$email&branch=$branch");
-        exit();
     }
     /*elseif($password !== $passwordRepeat){
         header("Location: signup.php?error=passwordcheck&username=".$username."&email=".$email);
@@ -42,43 +35,33 @@ if(isset($_POST['signup-submit'])){
     }*/
     else{
         $sql = "SELECT usnm FROM users WHERE usnm=?";
-        $stmt = mysqli_stmt_init($conn);
-        if(!mysqli_stmt_prepare($stmt, $sql)){
+        if ($stmt = $con->prepare($stmt)) {
             header("Location: signup.php?signup=sqlerror");
-            exit();
         }
         else{
-            mysqli_stmt_bind_param($stmt, "s", $username);
-            mysqli_stmt_execute($stmt);
-            mysqli_stmt_store_result($stmt);
-            $resultCheck = mysqli_stmt_num_rows($stmt);
-            if($resultCheck > 0){
-                //header("Location: login.php");
+            $stmt->bind_param('s', $username);
+            $stmt->execute();
+            $res = $stmt->get_result();
+            if($res->num_rows > 0){
                 header("Location: signup.php?signup=usertaken");
-                exit();
             }
             else{
-                //header("Location: login.php");
                 $sql = "INSERT INTO users (name, regno, email, usnm, pswd, branch) VALUES (?, ?, ?, ?, ?, ?)";
-                $stmt = mysqli_stmt_init($conn);
-                if(!mysqli_stmt_prepare($stmt, $sql)){
+                if($q = $con->prepare($sql)){
                     header("Location: signup.php?signup=sqlerror");
-                    exit();
                 }
                 else{
                     $hashedPwd = password_hash($password, PASSWORD_DEFAULT);
-
-                    mysqli_stmt_bind_param($stmt, "ssssss", $name, $regno, $email, $username, $hashedPwd, $branch);
-                    mysqli_stmt_execute($stmt);
+                    $q->bind_param('ssssss', $name, $regno, $email, $username, $hashedPwd, $branch);
+                    $q->execute();
                     header("Location: signup.php?signup=success");
-                    //header("Location: signup.php?signup=success");
-                    //exit();
+                    $q->close();
                 }
             }
+            $stmt->close();
         }
     }
-    mysqli_stmt_close($stmt);
-    mysqli_close($conn);
+    $con->close();
 }
 else{
     header("Location: signup.php");
